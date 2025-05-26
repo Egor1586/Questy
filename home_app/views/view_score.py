14178d3d9906bc4f5fd0f7b967c3cde9c9912b57
Zixther <duckandfiretto@gmail.com>
2025-05-26 20:55:57 +0300

    Finished homework

--- home_app/views/view_score.py
+++ home_app/views/view_score.py
@@ -1,6 +1,7 @@
 import flask
 import matplotlib.pyplot as plt
-import mpld3
+import io
+import base64
 
 from flask_login import current_user
 
@@ -11,35 +12,49 @@ from test_app.models import Test
 
 @render_page(template_name='score.html')
 def render_score():
+    fig, axes = plt.subplots()
+    buffer = io.BytesIO()
+    accuracy = []
+    dates_complete = []
+    list_user_answers = []
+    message = 'Вы не авторизованы'
+
     if current_user.is_authenticated:
-        scores = Score.query.limit(4).all() # тут ошибка
+
+        scores = Score.query.filter_by(user_id = 1).limit(4).all()
         test = Test.query.filter_by(id=scores[0].test_id).first()
-
-        # figure, axes = plt.subplots()
-
-        accuracy = []
-        dates_complete = []
-        list_user_answers = []
-
-
 
         for index in scores:
             accuracy.append(index.accuracy)
             dates_complete.append(index.date_complete)
             list_user_answers.append(index.user_answer.split("%$№"))
-        
+        print(f'Это аккуратность: {accuracy}')
+        accuracy[2] = 150
+        accuracy[3] = 1000
+        dates_complete[2] = '2025-01-30'
+        dates_complete[1] = '2025-01-25'
+        dates_complete[-1] = '2025-02-02'
+        if dates_complete[0] == dates_complete[-1]:
+            message = 'Слишком мало данных для построения графика'
+            return {'message': message}
 
-        print(f'Это список ответов: {list_user_answers}')
-        # axes.plot(dates_complete, accuracies, marker='o')
-        # html_str = mpld3.fig_to_html(figure)
-        # print(type(html_str))
+        axes.set_xlabel("Дата прохождения теста")
+        axes.set_ylabel("Точность %")
+        axes.set_title("Прогресс пользователя")
+        axes.plot(dates_complete, accuracy, marker='o')
+        plt.savefig(buffer, format='png')
+
+        buffer.seek(0)
+        image_bytes = buffer.read()
+        graph_to_html = base64.b64encode(image_bytes).decode('utf-8')
+        buffer.close()
 
         return {
             'accuracy': accuracy,
             'name_test': test.title,
             'dates_complete': dates_complete,
             'list_user_answers': list_user_answers[0],
-            # 'graphs': html_str
+            'graph': graph_to_html
         }
 
-    return {}
+    return {'message': message}
