66aa4f357ecb3d9d659f84565640b6cf544083d7
Egor1158 <egor115819@gmail.com>
2025-06-01 20:04:55 +0300

    New way passing test

--- test_app/views/view_passing_test.py
+++ test_app/views/view_passing_test.py
@@ -4,11 +4,11 @@ from ..models import Test, Quiz
 from flask_login import current_user
 from Project.render_page import render_page
 
-@render_page(template_name = 'passing_test.html')
+@render_page(template_name = 'current_question.html')
 def render_passing_test(test_code):
     list_answers= []
     list_quiz= []
-    user_answers= []
+    # user_answers= []
 
     test_id= flask.request.args.get("test_id")
 
@@ -18,23 +18,24 @@ def render_passing_test(test_code):
         list_answers.append(quiz.answer_options.split("%$№"))
         list_quiz.append(quiz)
 
-    if flask.request.method == "POST":
-        quiz_test_id = list_quiz[0].id
+    # if flask.request.method == "POST":
+    #     quiz_test_id = list_quiz[0].id
 
-        for id in range(test.total_questions):
-            if id != test.total_questions:
-                try:
-                    user_answers.append(flask.request.form[f'answers{quiz_test_id + id}'])
-                except Exception as error:
-                    user_answers.append("not answer")
+    #     for id in range(test.total_questions):
+    #         if id != test.total_questions:
+    #             try:
+    #                 user_answers.append(flask.request.form[f'answers{quiz_test_id + id}'])
+    #             except Exception as error:
+    #                 user_answers.append("not answer")
 
-        flask.session["user_answer"]= user_answers.copy()
-        user_answers.clear()
+    #     flask.session["user_answer"]= user_answers.copy()
+    #     user_answers.clear()
 
-        return flask.redirect(f"/result_test{test.test_code}?test_id= {test.id}")
+    #     return flask.redirect(f"/result_test{test.test_code}?test_id= {test.id}")
                 
     return {
         "test": test,
         "list_quiz": list_quiz,
-        "list_answers": list_answers
+        "list_answers": list_answers,
+        "question_numb": 1
     }
