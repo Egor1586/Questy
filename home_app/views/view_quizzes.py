30e3ff831a8da821f9292ee1e0cef7df3b1cdc46
Egor1158 <egor115819@gmail.com>
2025-07-28 12:41:51 +0300

    check room before start

--- home_app/views/view_quizzes.py
+++ home_app/views/view_quizzes.py
@@ -9,11 +9,23 @@ from flask_socketio import emit, join_room
 @render_page(template_name = 'quizzes.html')
 def render_quizzes():
     start_button= []
+    rooms_list= []
+    
     list_tests = Test.query.filter_by(author_name= current_user.username).all()
     count_of_tests = int(len(list_tests))
+
+    for test in list_tests:
+        ROOM = Room.query.filter_by(test_code = test.test_code).first()
+        if ROOM:
+            start_button.append(1)
+        else:
+            start_button.append(0)
+    
+    print(start_button)
     
     return{
     "list_tests": list_tests,
+    "start_button": start_button,
     "count_of_tests": count_of_tests
     }
 
