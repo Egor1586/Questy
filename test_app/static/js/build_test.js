41eb4a0103f94d20269b0fbdeaadaedd9ada3f84
Egor1158 <egor115819@gamil.com>
2025-09-07 19:41:30 +0300

    some changes

--- test_app/static/js/build_test.js
+++ test_app/static/js/build_test.js
@@ -1,3 +1,30 @@
+const contaiter = document.querySelector(".container");
+
 function buildTest(){
+    data = {
+        "topic": "",
+        "description": "",
+        "questions": [
+            {
+                "question_text": "",
+                "options": [
+                    ""
+                ],
+                "correct_answer": "",
+                "time": 0
+            }
+        ]
+    }
+
+    data["description"] = document.getElementById("test-description").value;
+    data["topic"] = document.getElementById("test-title").value;
+
     console.log("build test test")
+    console.log(data)
 }
+
+contaiter.addEventListener("click", function(event){
+    if (event.target.classList.contains("submit-button")){
+        buildTest()
+    }
+});
