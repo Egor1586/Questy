394c97a656714e03768a7c5e9de1b8ffc53797e9
Egor1586 <egor115819@gmail.com>
2025-10-26 19:44:01 +0200

    edit question fix

--- test_app/views/view_edit_quistion.py
+++ test_app/views/view_edit_quistion.py
@@ -1,77 +1,63 @@
-import flask, random
+import flask
+import random
 
 from Project.database import db
 from ..models import Test, Quiz
 from Project.render_page import render_page
 
-@render_page(template_name='edit_question.html')
+
+@render_page(template_name="edit_question.html")
 def render_edit_question():
+    quiz_id = flask.request.args.get("quiz_id")
+    test_id = flask.request.args.get("test_id")
 
-    list_answers = []
-    list_user_answers= []
+    quiz = Quiz.query.filter_by(id=quiz_id).first()
+    test = Test.query.filter_by(id=test_id).first()
 
-    answer_options = ""
-    correct_answer = "" 
-    answer = ""
+
+    list_answers = quiz.answer_options.split("%$№") if quiz.answer_options else []
+    correct_answer = quiz.correct_answer
     message = ""
 
-    quiz_id= flask.request.args.get("quiz_id")
-    test_id= flask.request.args.get("test_id")
+    if flask.request.method == "POST":
+        updated_answers = []
 
-    quiz = Quiz.query.filter_by(id = quiz_id).first()
-    test = Test.query.filter_by(id = test_id).first()
+        for i in range(test.answers_per_question):
+            field_name = f"answer{i}"
+            new_value = flask.request.form.get(field_name, "").strip()
 
-    list_answers = quiz.answer_options.split("%$№") 
-    correct_answer= quiz.correct_answer
-    
-    if flask.request.method == 'POST':
+            if new_value:
+                updated_answers.append(new_value)
+                print(f"==============={i}: {new_value}")
+            else:
+                if i < len(list_answers):
+                    updated_answers.append(list_answers[i])
+                    print(f"ОТВЕТЫ: {list_answers[i]}")
 
-        for number in range(test.answers_per_question):
-            try:
-                new_answer = flask.request.form.get(f'answer{number}', '')
-                if new_answer and new_answer != correct_answer:
-                    print(f"Новый ответа {new_answer}")
-                    list_user_answers.append(new_answer)
-                else:
-                    if list_answers[number] != correct_answer:
-                        print(f"Нет нового ответа {list_answers[number]}")
-                        list_user_answers.append(list_answers[number])
-
-            except Exception:
-                pass
-           
-        new_correct_answer = flask.request.form.get('correct_answer')
-        if new_correct_answer:
-            print(f"Есть новый правельный {new_correct_answer}")
-            list_user_answers.append(new_correct_answer)
-            quiz.correct_answer = new_correct_answer
+        new_correct = flask.request.form.get("correct_answer", "").strip()
+        if new_correct:
+            quiz.correct_answer = new_correct
+            if new_correct not in updated_answers:
+                updated_answers.append(new_correct)
+            print(f"ЕЩКЕРЕ: {new_correct}")
         else:
             quiz.correct_answer = correct_answer
-            print(f"Нет нового правельный {correct_answer}")
-            list_user_answers.append(correct_answer)
+            if correct_answer not in updated_answers:
+                updated_answers.append(correct_answer)
+            print(f"[не получилось: {correct_answer}")
 
-        print(list_user_answers)
-        random.shuffle(list_user_answers)
-        
-        print(list_user_answers)
-        print(new_correct_answer)
-        print(correct_answer)
-        for number, answer in enumerate(list_user_answers):
-            if number > 0:
-                answer_options+= f'%$№{answer}' 
-            else:
-                answer_options+= answer
-
-        print(answer_options)
-        quiz.answer_options = answer_options
+        random.shuffle(updated_answers)
+        quiz.answer_options = "%$№".join(updated_answers)
         db.session.commit()
-        return flask.redirect(f"/test_app?test_id={test.id}") 
-        
+
+        print(f"Вопроссы: {quiz.answer_options}")
+        return flask.redirect(f"/test_app?test_id={test.id}")
+
     return {
-        "test" : test,        
-        "quiz" : quiz,
-        "quiz_id" : quiz.id,
-        "list_answers" : list_answers,
-        "correct_answer" : quiz.correct_answer,
-        "message" : message
+        "test": test,
+        "quiz": quiz,
+        "quiz_id": quiz.id,
+        "list_answers": list_answers,
+        "correct_answer": correct_answer,
+        "message": message,
     }
