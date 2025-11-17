c4266031672b72cc13f69a7fb3dab95e477cfeb8
Egor1586 <egor115819@gmail.com>
2025-11-17 23:08:47 +0200

    merge test edit sviatoslav branch

--- home_app/views/view_edit_profile.py
+++ home_app/views/view_edit_profile.py
@@ -9,13 +9,15 @@ from Project.render_page import render_page
 
 @render_page(template_name = 'edit_profile.html')
 def render_edit_profile(user_id):
-    
-    user = User.query.filter_by(id= user_id).first()
+    if current_user.id == int(user_id):
+        user = User.query.filter_by(id= user_id).first()
 
-    if flask.request.method == "POST":
-        if flask.request.form.get("name"):
-            user.username = flask.request.form.get("name")
-            db.session.commit()
-            return flask.redirect("/profile")
+        if flask.request.method == "POST":
+            if flask.request.form.get("name"):
+                user.username = flask.request.form.get("name")
+                db.session.commit()
+                return flask.redirect("/profile")
+    else:
+        return flask.redirect("/profile")
     
     return { }
